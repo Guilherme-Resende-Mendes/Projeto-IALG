9f421cc1ee5ea608cf14279e80e04cd83467f9a2
Guilherme-Resende-Mendes <135715433+Guilherme-Resende-Mendes@users.noreply.github.com>
2023-06-17 23:14:29 -0300

    Update exe1 (2).cpp
    Coisas que o Xande mexeu

--- exe1 (2).cpp
+++ exe1 (2).cpp
@@ -1,26 +1,55 @@
 #include <iostream>
 #include <fstream>
-#include <Windows.h>
+#include <limits>
 using namespace std;
 
 struct pessoa{
 	string nome;
-	int cpf;
+	string cpf;
 	string tipoSanguineo;
 	int porcentVermelho;
 	int porcentBranco;
-    int densidade;
+    float densidade;
 };
-void cadastrarpessoa(fstream& arquivoOut) {
+
+
+int cadastro(fstream& arquivo, pessoa cadastros[]){
+    string lixo;
+
+    cout << "** Homogramas Cadastrados **" << endl;
+    for(int i=0; i<5; i++){
+        getline(arquivo, cadastros[i].nome, ';');
+        getline(arquivo, cadastros[i].cpf, ';');
+        getline(arquivo, cadastros[i].tipoSanguineo, ';');
+        arquivo >> cadastros[i].porcentVermelho;
+        arquivo >> lixo;
+        arquivo >> cadastros[i].porcentBranco;
+        arquivo >> lixo;
+        arquivo >> cadastros[i].densidade;
+        arquivo.ignore();
+
+        cout << cadastros[i].nome << endl;
+        cout << cadastros[i].cpf << endl;
+        cout << cadastros[i].tipoSanguineo << endl;
+        cout << cadastros[i].porcentVermelho << "%" << endl;
+        cout << cadastros[i].porcentBranco << "%" << endl;
+        cout << cadastros[i].densidade << endl;
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+void cadastrarNovaPessoa(fstream& arquivoOut) {
     pessoa novaPessoa;
 
     cout << "** CADASTRO DE PESSOA **" << endl;
     cout << "Nome: " << endl;
-    cin >> novaPessoa.nome;
-    cout << "CPF: ";
-    cin >> novaPessoa.cpf;
+    getline(cin, novaPessoa.nome);
+    cout << "CPF: " << endl;
+    getline(cin, novaPessoa.cpf);
     cout << "Tipo Sanguineo: "<< endl;
-    cin.ignore();
     getline(cin, novaPessoa.tipoSanguineo);
     cout << "Porcentagem de Globulos Vermelhos: " << endl;
     cin>> novaPessoa.porcentVermelho;
@@ -43,30 +72,32 @@ void cadastrarpessoa(fstream& arquivoOut) {
 }
 
 
-void buscarpessoa(fstream& arquivo){
-	pessoa nomeProcurado;
+void buscarpessoa(fstream& arquivo, pessoa nomeProcurado[]){
 	bool encontrar = false;
+    int indice;
 	string nome;
 	cout<< "Digite o nome da pessoa: ";
 	cin>> nome;
 
-while ((!encontrar) and (arquivo >> nomeProcurado.nome >> nomeProcurado.cpf >> nomeProcurado.tipoSanguineo >> nomeProcurado.porcentVermelho >> nomeProcurado.porcentBranco >> nomeProcurado.densidade)) {
-    cout << "Lendo pessoa: " << nomeProcurado.nome << endl;
-        if (nomeProcurado.nome == nome) {
-            encontrar = true;
-        } 
-        
+    for(int i=0; i<5; i++){
+        while ((!encontrar) and (arquivo >> nomeProcurado[i].nome >> nomeProcurado[i].cpf >> nomeProcurado[i].tipoSanguineo >> nomeProcurado[i].porcentVermelho >> nomeProcurado[i].porcentBranco >> nomeProcurado[i].densidade)) {
+            cout << "Lendo pessoa: " << nomeProcurado[i].nome << endl;
+            if (nomeProcurado[i].nome == nome) {
+                encontrar = true;
+                indice = i;
+            } 
+            
+        }
     }
-   
 
 	if (encontrar){
 		cout << "** PESSOA ENCONTRADA **" << endl;
-        cout << "Nome: " << nomeProcurado.nome << endl;
-        cout << "CPF: " << nomeProcurado.cpf << endl;
-        cout << "Tipo Sanguineo: " << nomeProcurado.tipoSanguineo << endl;
-        cout << "Porcentagem Globulos Vermelhos: " << nomeProcurado.porcentVermelho <<"%"<< endl;
-        cout << "Porcentagem Globulos Brancos: " << nomeProcurado.porcentBranco <<"%"<< endl;
-        cout << "Densidade do Sangue: " << nomeProcurado.densidade << endl;
+        cout << "Nome: " << nomeProcurado[indice].nome << endl;
+        cout << "CPF: " << nomeProcurado[indice].cpf << endl;
+        cout << "Tipo Sanguineo: " << nomeProcurado[indice].tipoSanguineo << endl;
+        cout << "Porcentagem Globulos Vermelhos: " << nomeProcurado[indice].porcentVermelho <<"%"<< endl;
+        cout << "Porcentagem Globulos Brancos: " << nomeProcurado[indice].porcentBranco <<"%"<< endl;
+        cout << "Densidade do Sangue: " << nomeProcurado[indice].densidade << endl;
 	}
 	else{
 		cout << endl << "Pessoa não encontrada" << endl;
@@ -75,62 +106,64 @@ while ((!encontrar) and (arquivo >> nomeProcurado.nome >> nomeProcurado.cpf >> n
 }
 
 
-void alterarpessoa(fstream& arquivo){
-	pessoa nomeParaTrocar;
+void alterarpessoa(fstream& arquivo, pessoa nomeParaTrocar[]){
 	bool encontrar = false;
-	string nome;
-	cout<< "Digite o indentificador do animal a ser alterado: ";
-	cin>> nome;
-	while(!encontrar and arquivo >> nomeParaTrocar.nome >> nomeParaTrocar.cpf >> nomeParaTrocar.tipoSanguineo >> nomeParaTrocar.porcentVermelho >> nomeParaTrocar.porcentBranco ){
-		if(nomeParaTrocar.nome == nome){
-			encontrar=true;
-		}
-	}
+    int indice;
+	string cpf;
+	cout<< "Digite o CPF do paciente a ser alterado: ";
+	cin>> cpf;
+
+    for(int i=0; i<5; i++){
+        while(!encontrar and (arquivo >> nomeParaTrocar[i].nome >> nomeParaTrocar[i].cpf >> nomeParaTrocar[i].tipoSanguineo >> nomeParaTrocar[i].porcentVermelho >> nomeParaTrocar[i].porcentBranco)){
+            if(nomeParaTrocar[i].cpf == cpf){
+                encontrar=true;
+                indice = i;
+            }
+        }
+    }
 	if (encontrar){
 		cout << "PESSOA ENCONTRADA" << endl;
-        cout << "Nome: " << nomeParaTrocar.nome << endl;
-        cout << "CPF: " << nomeParaTrocar.cpf << endl;
-        cout << "Tipo Sanguineo: " << nomeParaTrocar.tipoSanguineo << endl;
-        cout << "Porcentagem de Globulos Vermelhos: " << nomeParaTrocar.porcentVermelho <<"%" << endl;
-        cout << "Porcentagem de Globulos Brancos: " << nomeParaTrocar.porcentBranco <<"%"<<  endl;
-        cout << "Densidade do Sangue: " << nomeParaTrocar.densidade << endl;
+        cout << "Nome: " << nomeParaTrocar[indice].nome << endl;
+        cout << "CPF: " << nomeParaTrocar[indice].cpf << endl;
+        cout << "Tipo Sanguineo: " << nomeParaTrocar[indice].tipoSanguineo << endl;
+        cout << "Porcentagem de Globulos Vermelhos: " << nomeParaTrocar[indice].porcentVermelho <<"%" << endl;
+        cout << "Porcentagem de Globulos Brancos: " << nomeParaTrocar[indice].porcentBranco <<"%"<<  endl;
+        cout << "Densidade do Sangue: " << nomeParaTrocar[indice].densidade << endl;
 	}
 
 
-    cout << "** ALTERAÇÃO DE HOMOGRAMA **" << endl;
+    cout << "** ALTERAÇÃO DE HeMOGRAMA **" << endl;
     cout << "Nome: " << endl;
-    cin >> nomeParaTrocar.nome;
+    getline(cin, nomeParaTrocar[indice].nome);
     cout << "CPF: ";
-    cin >> nomeParaTrocar.cpf;
+    getline(cin, nomeParaTrocar[indice].cpf);
     cout << "Tipo Sanguineo: "<< endl;
     cin.ignore();
-    getline(cin, nomeParaTrocar.tipoSanguineo);
+    getline(cin, nomeParaTrocar[indice].tipoSanguineo);
     cout << "Porcentagem de Globulos Vermelhos: " << endl;
-    cin>> nomeParaTrocar.porcentVermelho;
+    cin>> nomeParaTrocar[indice].porcentVermelho;
     cout << "Porcentagem de Globulos Brancos: " << endl;
-    cin >> nomeParaTrocar.porcentBranco;
+    cin >> nomeParaTrocar[indice].porcentBranco;
     cout << "Densidade do sangue: " << endl;
-    cin >> nomeParaTrocar.densidade;
+    cin >> nomeParaTrocar[indice].densidade;
 
-	arquivo << nomeParaTrocar.nome << endl;
-    arquivo << nomeParaTrocar.cpf << endl;
-    arquivo << nomeParaTrocar.tipoSanguineo << endl;
-    arquivo << nomeParaTrocar.porcentVermelho << endl;
-    arquivo << nomeParaTrocar.porcentBranco << endl;
-    arquivo << nomeParaTrocar.densidade << endl;
 }
 
 
+void excluirpessoa(fstream& arquivo, pessoa cadastros[]){
 
+}
 
 
 int main() {
-    SetConsoleOutputCP(CP_UTF8);
     fstream arquivo("animais_exoticos.txt");
+    pessoa cadastros[5];
 
     if (!arquivo) {
         cout << "Erro ao abrir o arquivo!" << endl;
         return 1;
+    }else {
+        cadastro(arquivo, cadastros);
     }
 
     int opcao;
@@ -140,21 +173,23 @@ int main() {
         cout << "2. Buscar pessoa" << endl;
         cout << "3. Alterar dados de uma pessoa" << endl;
         cout << "4. Excluir dados de uma pessoa" << endl;
+	    cout << "5. Sair" << endl;
         cout << "Escolha uma opção: ";
         cin >> opcao;
 
         switch (opcao) {
             case 1:
-                cadastrarpessoa(arquivo);
+                cadastrarNovaPessoa(arquivo);
                 break;
             case 2:
-                buscarpessoa(arquivo);
+                buscarpessoa(arquivo, cadastros);
                 break;
             case 3:
-                alterarpessoa(arquivo);
+                alterarpessoa(arquivo, cadastros);
                 break;
             case 4:
-                excluirpessoa(arquivo);
+                excluirpessoa(arquivo, cadastros);
+                break;
             case 5:
                 cout << "Saindo do programa..." << endl;
                 break;
@@ -164,7 +199,7 @@ int main() {
         }
 
         cout << endl;
-    } while (opcao != 4);
+    } while (opcao != 5);
 
     arquivo.close();
 
